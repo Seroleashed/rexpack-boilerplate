a40c6945b3452a247751a46b03d145449e4e6659
Seroleashed <dsilorenz@gmail.com>
2019-08-17 20:57:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./js/App";
-import "./css/style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.scss";
 import "./js/libs/holder.min.js";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -14,7 +14,8 @@ ReactDOM.render(
   document.getElementById("react-container") // eslint-disable-line no-undef
 );
 // Needed for Hot Module Replacement
-// eslint-disable-line no-undef
+/* eslint-disable */
 if (typeof module.hot !== "undefined") {
-  module.hot.accept(); // eslint-disable-line no-undef
+  module.hot.accept();
 }
+/* eslint-enable */
