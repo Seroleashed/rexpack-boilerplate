fe32a56ae8b68270a5db9b09156848b140f8ff32
Seroleashed <dsilorenz@gmail.com>
2019-08-18 14:24:25 +0200

    Update App.js
    add Error 404 page

--- src/js/App.js
+++ src/js/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import Page404 from "./pages/Page404";
 
 class App extends React.Component {
   constructor(...args) {
@@ -13,6 +14,7 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route component={Page404} />
         </Switch>
       </div>
     );
